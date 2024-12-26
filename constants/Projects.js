a314df8e3a690f2a84a94e14b14ae4a5914adaa5
AEmre23 <aemrea23@gmail.com>
2024-12-26 21:59:05 +0300

    feat: add grid and radial overlay animations

---
+++ constants/Projects.js
@@ -0,0 +1,78 @@
+import derimod from "@/public/derimod.png";
+import jumbo from "@/public/jumbo.png";
+import karaca from "@/public/karaca.png";
+import purest from "@/public/purest.png";
+import ilacsiz from "@/public/ilacsiz.png";
+import sloth from "@/public/sloth.png";
+import blog from "@/public/blog.png";
+import pop from "@/public/pop.png";
+import emre from "@/public/emre.png";
+import country from "@/public/country.png";
+
+export const projects = [
+    {
+      title: "Derimod",
+      description: "E-commerce website for a local company",
+      linksite: "https://derimod.com.tr/",
+      image: derimod,
+    },
+    {
+      title: "Jumbo",
+      description: "E-commerce website for a local company",
+      linksite: "https://www.jumbo.com.tr/",
+      image: jumbo,
+    },
+    {
+      title: "Karaca",
+      description: "E-commerce website for a local company",
+      linksite: "https://www.karaca.com.de/",
+      image: karaca,
+    },
+    {
+      title: "The Purest Solutions",
+      description: "E-commerce website for a local company",
+      linksite: "https://thepurestsolutions.com/",
+      image: purest,
+    },
+    {
+      title: "Ilacsiz Yasam",
+      description: "E-commerce website for a local company",
+      linksite: "https://www.ilacsizyasam.com/",
+      image: ilacsiz,
+    },
+    {
+      title: "Sloth Bedding",
+      description: "E-commerce website for a local company",
+      linksite: "https://slothbedding.com/",
+      image: sloth,
+    },
+    {
+      title: "Emre Blog",
+      description: "My personal blog",
+      linksite: "https://emre-blog.vercel.app/",
+      linkcode: "https://github.com/AEmre23/my-blog",
+      image: blog,
+    },
+    {
+      title: "Popup Generator",
+      description: "Popup generator for websites",
+      linksite: "https://popup-generator-emre.vercel.app/",
+      linkcode: "https://github.com/AEmre23/popup-generator",
+      image: pop,
+    },
+    {
+      title: "Emre Portfolio",
+      description: "My portfolio website",
+      linksite: "https://www.emrealtunkaya.dev/",
+      linkcode: "https://github.com/AEmre23/portfolio-website",
+      here: true,
+      image: emre,
+    },
+    {
+      title: "Countries Info App",
+      description: "Countries info app",
+      linksite: "https://countries-info-app-opal.vercel.app/",
+      linkcode: "https://github.com/AEmre23/countries-info-app",
+      image: country,
+    },
+];
